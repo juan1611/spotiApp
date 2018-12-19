e7ea9a596b48efdda0c51a69e42e70bc74b2e2ad
Juan Gabriel Velásquez Ortiz <jgvelort1611@gmail.com>
2018-12-19 13:43:10 -0500

    Revert "Commit Inicial"
    This reverts commit 8f8c4f3dac14614428e54cb5487ec8f0bfdbb4fc.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,11 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { SearchComponent } from './components/search/search.component';
-import { ArtistaComponent } from './components/artista/artista.component';
-import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    SearchComponent,
-    ArtistaComponent,
-    NavbarComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
